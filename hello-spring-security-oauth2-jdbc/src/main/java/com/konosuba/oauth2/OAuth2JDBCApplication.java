bf38fa182d597428e5aa22c72ff7a1c3a1ce4329
xiongtianle <2529814288@qq.com>
2020-08-31 15:26:03 +0800

    基于JDBC存储令牌，以后都是使用这种方式获取存储令牌

---
+++ hello-spring-security-oauth2-jdbc/src/main/java/com/konosuba/oauth2/OAuth2JDBCApplication.java
@@ -0,0 +1,22 @@
+package com.konosuba.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 认证服务器，用于获取 Token
+ * <p>
+ * Description:
+ * </p>
+ *
+ * @author konosuba
+ * @version v1.0.0
+ * @date 2020.8.29
+ * @see com.konosuba.oauth2
+ */
+@SpringBootApplication
+public class OAuth2JDBCApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2JDBCApplication.class, args);
+    }
+}
